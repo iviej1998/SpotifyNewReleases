10204d59d7f7aae27ea2997754b525b0c4063f23
Jillian Ivie <iviej@my.erau.edu>
2025-04-05 13:46:18 -0700

    update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -25,3 +25,50 @@ class Test(TestCase):
     def test_fetch_new_releases_button(self, mock_get_new_releases: MagicMock) -> None:
         """ This function tests if Fetch New Releases button works and displays mocked albums """
         
+        #set up mock test to get the new releases from spotify
+        mock_get_new_releases.return_value= [
+            {
+                "name" : "Mock Album",
+                "artists" : [{"name": "Mock Artist"}],
+                "release_date": "2024-04-19",
+                "images": [{"url": "https://example.com/image.jpg"}],
+                "id": "123"
+            }
+        ]
+        at = AppTest.from_file(".../src/app.py") #load the streamlit app fromthe specified file for testing
+        
+        #set session state to simulate authorized access
+        at.session_state["access_token"] = "mock_token"
+        at.session_state["refresh_token"] = "mock_refresh"
+        at.session_state["tokens_exchanged"] = True
+
+        at.run() #simulate streamlit's starup state based on current session state
+        at.button("Fetch New Releases").click().run() #simulate "Fetch New Releases" button
+
+        assert "Found 1 albums!" in at.markdown[0].values
+        assert "Mock Album" in at.subheader[0].values
+
+    @patch("src.app.refresh_access_token")
+    def test_manual_refresh_button(self, mock_refresh_access_token):
+        """ This functions tests if Refresh Access Token Manually button updates the access token"""
+        
+        #set up mock test to refresh the access token
+        mock_refresh_access_token.return_value = {
+            "access_token": "new_mock_token",
+            "expires_in": 3600
+        }
+
+        at = AppTest.from_file("../src/app.py")
+        
+        #set session state to test manual refresh feature
+        at.session_state["access_token"] = "old_token"
+        at.session_state["refresh_token"] = "mock_refresh"
+        at.session_state["tokens_exchanged"] = True
+        at.session_state["expires_in"] = 3600
+        at.session_state["token_timestamp"] = 0
+
+        at.run()
+        at.button("Refresh Access Token Manually").click().run() #simulate clicking the "Refresh Access Token Manually" and rerun the app
+
+        #confirm the manual refresh logic updates the session state access token to the new mocked value
+        assert at.session_state["access_token"] == "new_mock_token"
